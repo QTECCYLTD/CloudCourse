fadfee9481350c9e2f7ca76ccb3d674fca70d886
Adnan Farra <adnan@qteccy.com>
2023-10-19 14:58:06 +0200

    First Go Commit

---
+++ Lec2/helloworld.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var N int = 5
+	var X int = 5
+	var FName string = "Adnan"
+	var LName string = "Farra"
+
+	for i := 0; i < N; i++ {
+		fmt.Println(FName)
+	}
+	for i := 0; i < X; i++ {
+		fmt.Println(LName)
+	}
+
+}
