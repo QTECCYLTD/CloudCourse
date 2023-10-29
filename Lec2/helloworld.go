21cbe29383c932f229622bea5248d163d721d0b6
Adnan Farra <adnan@qteccy.com>
2023-10-29 20:55:07 +0100

    changed due to task change

--- Lec2/helloworld.go
+++ Lec2/helloworld.go
@@ -1,21 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	var N int = 5
-	var X int = 5
-	var FName string = "Adnan"
-	var LName string = "Farra"
 
-	for i := 0; i < N; i++ {
-		fmt.Println(FName)
-	}
-	for i := 0; i < X; i++ {
-		fmt.Println(LName)
-		//test commit comment
-
-		// Gitlens test commit
-	}
+	println("Hello World!")
 
 }
